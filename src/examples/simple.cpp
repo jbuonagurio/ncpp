663c331047f15cc3ab79c3abeaa1ec6b256f896c
John Buonagurio <jbuonagurio@exponent.com>
2019-04-27 01:09:48 -0400

    Update example to show selection API; add auto-link header for MSVC.

--- src/examples/simple.cpp
+++ src/examples/simple.cpp
@@ -1,5 +1,5 @@
 /**
- * Sample command line build using Visual Studio 2017:
+ * Sample command line build using MSVC:
  *
  * cl simple.cpp /EHsc /MT /std:c++17 /I..\..\include /I..\..\deps\include /link /LIBPATH:..\..\deps\lib
  */
@@ -14,7 +14,7 @@
 #include <ncpp/ncpp.hpp>
 
 // Automatically link static libraries for MSVC.
-#include <auto_link.h>
+#include "auto_link.h"
 
 // Helper to print the contents of a variant.
 static std::ostream& operator<<(std::ostream& os, const ncpp::variant& v)
@@ -48,7 +48,6 @@ static std::ostream& operator<<(std::ostream& os, const ncpp::attribute& rhs)
 int main(int argc, char *argv[])
 {
     // Using sea surface temperature example from Unidata.
-    
     std::string filename;
     if (argc > 1)
         filename = argv[1];
@@ -69,7 +68,14 @@ int main(int argc, char *argv[])
         
         std::cout << "\nVARIABLES:\n";
         for (const auto& var : ds.vars) {
-            std::cout << "\t" << var.name() << "\n";
+            std::cout << "\t" << var.name() << "(";
+            std::string separator;
+            for (const auto& dim : var.dims) {
+                std::cout << separator << dim.name();
+                separator = ",";
+            }
+            std::cout << ")\n";
+            
             for (const auto& att : var.atts) {
                 std::cout << "\t\t" << att << "\n";
             }
@@ -80,13 +86,50 @@ int main(int argc, char *argv[])
             std::cout << "\t" << att << "\n";
         }
         
-        // Print the time variable array.
-        std::cout << "\nTIME VARIABLE:\n";
-        auto time = ds.vars["time"].value<int>();
-        std::cout << "\t" << time << "\n";
+        // Coordinate variables for dimensions.
+        std::cout << "\nCOORDINATES TEST:\n";
+        auto time = ds.dims["time"].coordinates<int>();
+        std::cout << "time: \t" << time << "\n\n";
+        auto lat = ds.dims["lat"].coordinates<double>();
+        std::cout << "lat: \t" << lat << "\n\n";
+        auto lon = ds.dims["lon"].coordinates<double>();
+        std::cout << "lon: \t" << lon << "\n\n";
+        
+        // Selection API.
+        std::cout << "\nSELECTION TEST:\n";
+        
+        auto sst = ds.vars["tos"];
+        auto slice = sst
+            .select<double>({
+                {"time", 375, 375},    // time(12)
+                {"lat", -15.5, -15.5}, // lat(64)
+                {"lon", 125, 125}      // lon(62)
+            });
+        
+        std::cout << "shape:";
+        for (const auto& i : slice.shape()) {
+            std::cout << " " << i;
+        }
+        std::cout << "\n";
+        
+        auto coordinates = slice.coordinates<int, double, double>();
+        auto values = slice.values<double>();
+
+        for (int i = 0; i < coordinates.size(); ++i) {
+            auto c = coordinates.at(i);
+            auto v = values.at(i);
+            std::cout << "[" << std::get<0>(c) 
+                      << "," << std::get<1>(c)
+                      << "," << std::get<2>(c)
+                      << "]\t" << std::fixed << std::setprecision(4) << v << "\n";
+        }
     }
     catch (std::system_error& e) {
         std::cerr << e.code() << "\n";
+        std::cerr << e.what() << "\n";
+        return 1;
+    }
+    catch (std::exception& e) {
         std::cerr << e.what() << "\n";
         return 1;
     }
